9a5a4900db240dfc7025572310c519f7dbcb4f98
RuChuanLin <ruchuanlin@gmail.com>
2017-11-14 10:20:36 +0800

    update

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get('/', (req, res) => {
   res.send({ hi: 'there' });
 });
 
-app.listen(5000);
+// 指定環境埠號。部署時會某些server provider會動態指定埠號，因此必須要由process.env.PORT
+// 來讀取。預設埠號5000，給開發環境使用。
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
