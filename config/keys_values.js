fe41d872fa05b8aa2316e81454a95ea4a7b4bc97
RuChuanLin <ruchuanlin@gmail.com>
2017-11-15 08:36:08 +0800

    Delete keys_values.js

--- config/keys_values.js
+++
@@ -1,9 +0,0 @@
-// (重要!!) 需要在本檔案中加入keys.js的所有需要keys
-// 直接複製module.exports裡的屬性即可
-module.exports = {
-  googleClientID:
-    '364296180136-o45i36dfiqv02k3mk4d2llg985qg9kq7.apps.googleusercontent.com',
-  googleClientSecret: 'KoelpXIl_2jmvidQvDQo0nTb',
-  mongoURI:
-    'mongodb://ac97631:123qweasd@ds259325.mlab.com:59325/emaily543292-dev'
-};
